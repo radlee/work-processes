6a5786c80dd74f0e7c51dd5b60d62e4617caa4da
lolito <prof.lolito@gmail.com>
2023-10-16 19:28:41 +0200

    Need to find the bug

--- server/routes/main.js
+++ server/routes/main.js
@@ -12,13 +12,31 @@ const Post = require('../models/Post');
 //Routes
 router.get('', async(req, res) => {
 
-    const locals = {
-        title: 'GSSC Processes Blog',
-        description: 'Call Centre Processes Simplified'
-    }
     try {
-        const data = await Post.find();
-        res.render('index', { locals, data });
+        const locals = {
+            title: 'GSSC Processes Blog',
+            description: 'Call Centre Processes Simplified'
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        // const data = await Post.find();
+        res.render('index', { 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
     } catch (error) {
         console.log(error)
     }
@@ -34,28 +52,31 @@ router.get('/about', (req, res) => {
 });
 
 
-// function insertPostData () {
-//     Post.insertMany([
-//         {
-//             title: 'Building a Blog',
-//             body: 'A very short story'
-//         },
-//         {
-//             title: 'User Not Permitted to LogIn',
-//             body: 'Processes for Device related issues'
-//         },
-//         {
-//             title: 'Adding a Beneficiary',
-//             body: 'Propper structure for adding a beneficiary.'
-//         },
-//         {
-//             title: 'Ria Sikhona',
-//             body: 'What is Ria Sikhona'
-//         },
-//     ])
-// }
 
-// insertPostData()
+/**
+ * GET
+ * Post: id
+ */
+
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const locals = {
+            title: 'GSCC Processes',
+            description: 'The complete guide to processes'
+        }
+
+        let slug = req.params.id;
+
+        const data = await Post.findById({ _id: slug  });
+        res.render('index', { locals, data });
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
 
 
 
