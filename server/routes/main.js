d37c70d285c1bfea5012cc4ea347508271d1c0ee
lolito <prof.lolito@gmail.com>
2023-10-16 15:45:53 +0200

    Databases Connection Setup

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,18 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+
+
+
+/**
+ * GET
+ * HOME
+ */
 
 //Routes
-router.get('', (req, res) => {
+router.get('', async(req, res) => {
 
     const locals = {
         title: 'GSSC Processes Blog',
         description: 'Call Centre Processes Simplified'
     }
-    res.render('index', { locals });
+    try {
+        const data = await Post.find();
+        res.render('index', { locals, data });
+    } catch (error) {
+        console.log(error)
+    }
 });
+
+
+
+
+
 
 router.get('/about', (req, res) => {
     res.render('about');
 });
+
+
+// function insertPostData () {
+//     Post.insertMany([
+//         {
+//             title: 'Building a Blog',
+//             body: 'A very short story'
+//         },
+//         {
+//             title: 'User Not Permitted to LogIn',
+//             body: 'Processes for Device related issues'
+//         },
+//         {
+//             title: 'Adding a Beneficiary',
+//             body: 'Propper structure for adding a beneficiary.'
+//         },
+//         {
+//             title: 'Ria Sikhona',
+//             body: 'What is Ria Sikhona'
+//         },
+//     ])
+// }
+
+// insertPostData()
+
+
 
 module.exports = router;
