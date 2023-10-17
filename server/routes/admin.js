309adcb22ab07ea6b7a3b374204a75b0e62f389f
radlee <prof.lolito@gmail.com>
2023-10-17 23:02:29 +0200

    Added Register and Sessions

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
 
 /**
  * GET
@@ -32,8 +35,57 @@ router.get('/admin', async (req, res) => {
   router.post('/admin', async (req, res) => {
     try {
         const { username, password } = req.body;
-        console.log(req.body)
-      res.redirect('/admin')
+
+
+        const user = await User.findOne( { username });
+
+        if(!user) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, jwtSecret);
+        res.cookie('token', token, { httpOnly: true });
+        res.redirect('/dashboard');
+      
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+
+    /**
+ * GET
+ * Admin - Dashboard
+ */
+  router.get('/dashboard', async (req, res) => {
+    res.render('admin/dashboard');
+  });
+
+  /**
+ * POST
+ * Admin - Register
+ */
+
+  router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+          const user = await User.create({ username, password: hashedPassword })
+          res.status(201).json({ message: 'User Created Successfully.', user })
+        } catch (error) {
+          if(error.code === 11000) {
+            res.status(409).json({ message: 'User already in use.' })
+          }
+          res.status(500).json({ message: 'Internal Server Error.' })
+        }
     } catch (error) {
       console.log(error);
     }
